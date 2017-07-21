82e62685ffe8c24a78c4f3a652024318f759d8d4
Sungin Jung <jungs@uchicago.edu>
2017-07-21 12:56:31 -0500

    fininsh exam

---
+++ WebFundamentals/week2/birthday.js
@@ -0,0 +1,15 @@
+var daysUntilMyBirthday = 60;
+
+function countDown (daysRemaining) {
+    while (daysRemaining > 0) {
+        if (daysRemaining >= 30) {
+            console.log(daysRemaining + " days left. Such a long time...")
+        } else if (daysRemaining < 30 && daysRemaining > 5) {
+            console.log(daysRemaining + "days left. It's getting close!")
+        } else if (daysRemaining >= 1 && daysRemaining <= 5) {
+            console.log("Only " + daysRemaining + " days left!!!!!!!!!!!!!!!!!!!")
+        }
+        daysRemaining--;
+    }
+    console.log("HAPPY BIRTHDAY!!!!!!!!!!!!!!!")
+}
