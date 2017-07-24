d24f701f15caf8aefc75d47e5687861e3d91529c
Sungin Jung <jungs@uchicago.edu>
2017-07-24 17:57:12 -0500

    finish debuggin assignment

--- WebFundamentals/week2/NinjaToCat/ninjatocat.js
+++ WebFundamentals/week2/NinjaToCat/ninjatocat.js
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 $(document).ready(function () {
     $("img").click(function (){
             var temp = $(this).attr("src");
