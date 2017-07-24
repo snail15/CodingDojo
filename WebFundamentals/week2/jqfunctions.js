5aa4401ef06a4c51b30988e46715f9d578fa2f60
Sungin Jung <jungs@uchicago.edu>
2017-07-24 12:22:30 -0500

    implement up to slideToggle

--- WebFundamentals/week2/jqfunctions.js
+++ WebFundamentals/week2/jqfunctions.js
@@ -20,4 +20,22 @@
                         $(".hidden-toggle-list").hide(800);
                     })
                 })
+
+                $(".slide-down-btn").click(function () {
+                    $("#slide-down-img").slideDown(1200, function() {
+                        $("#slide-down-img").hide(800);
+                    });
+                })
+
+                $(".slide-up-btn").click(function () {
+                    $("#slide-up-img").slideUp(1500, function() {
+                        $("#slide-up-img").slideDown(800);
+                    });
+                })
+
+                $(".slide-toggle-btn").click(function () {
+                    $("#slide-toggle-img").slideToggle(1500);
+                })
+
+
             });
