c2631c8257a30316372fac2b83037b84466541d2
Sungin Jung <jungs@uchicago.edu>
2017-07-25 16:13:27 -0500

    finish functinalities of contact card assignment

---
+++ WebFundamentals/week2/ContactCards/contactcards.js
@@ -0,0 +1,34 @@
+function attachDescriptionHandler(description, firstName, lastName) {
+    var generalCard = true;
+
+    $(".card").click(function (){
+        if (generalCard == true) {
+            $(this).html("<p>"+description+"</p>");
+            $(this).children("p").addClass("description-p")
+            generalCard = false;
+        } else {
+            $(this).html("<h3>"+firstName+" "+lastName
+                        +"</h3>"
+                        +"<p>Click here for the description!</p>");
+            generalCard = true;
+        }
+    })
+}
+
+$(document).ready(function () {
+    
+     $(".submit").click(function(){
+        var firstName = $("input[name=first_name]").val();
+        var lastName = $("input[name=last_name]").val();
+        var description = $("#description").val();
+        
+        $(".card-col").append("<div class='card'>"
+                                +"<h3>"+firstName+" "+lastName
+                                +"</h3>"
+                                +"<p>Click here for the description!</p>");
+        
+        attachDescriptionHandler(description, firstName, lastName);
+    
+    })
+
+});
