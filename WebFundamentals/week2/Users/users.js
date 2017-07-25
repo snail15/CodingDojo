6b0e88ecb18ae32efa25e077318c7dda2766211e
Sungin Jung <jungs@uchicago.edu>
2017-07-25 14:21:30 -0500

    fix bug for user assignment

--- WebFundamentals/week2/Users/users.js
+++ WebFundamentals/week2/Users/users.js
@@ -2,13 +2,17 @@ $(document).ready(function () {
 
     $(".submit").click(function(){
         var firstName = $("input[name=first_name]").val();
-        var lasttName = $("input[name=last_name]").val();
+        var lastName = $("input[name=last_name]").val();
         var email = $("input[name=email]").val();
         var contactNo = $("input[name=contact]").val();
         
-        $("#usertable > tbody:last-child").append("<tr><td>"+firstName
-        +"</td><td>"+lastName+"</td><td>"
-        +email+"</td><td>"
-        +contactNo+"</td></tr>");
+        $("#tbody").append("<tr>"
+            +"<td>"+firstName
+            +"</td><td>"+lastName
+            +"</td><td>"+email
+            +"</td><td>"+contactNo
+            +"</td></tr>");
+        
+        // $("tbody").append("<tr><td>Bill</td><td>Gates</td><td>ads@asdas.com</td><td>123-456-7890</td></tr>");
     })
 });
