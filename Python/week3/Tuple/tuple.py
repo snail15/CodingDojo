d43dc1a2f405de2dfc8b9835262b7b2c09ff56d3
Sungin Jung <jungs@uchicago.edu>
2017-07-31 16:36:34 -0500

    finish Tuple assignment

---
+++ Python/week3/Tuple/tuple.py
@@ -0,0 +1,16 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def makeTuple(dict):
+    newArr = []
+    for key in dict:
+        tup = ()
+        tup.add(key)
+        tup.add(dict[key])
+        newArr.append(tup)
+    print(newArr)
+
+makeTuple(my_dict)
