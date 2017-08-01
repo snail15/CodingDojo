67b3344a04fd293dc020167255ae540f87f34c5b
Sungin Jung <jungs@uchicago.edu>
2017-08-01 17:03:19 -0500

    finish flask name  page

---
+++ Python/flask/flask_fundamental/DojoSurvey/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template("index.html")
+
+@app.route('/result', methods=["POST"])
+def result():
+    
+    name = request.form["name"]
+    location = request.form["location"]
+    language = request.form["language"]
+    comment = request.form["comment"]
+
+    return render_template("result.html", name=name, location=location, language=language, comment=comment)
+
+app.run(debug=True)
