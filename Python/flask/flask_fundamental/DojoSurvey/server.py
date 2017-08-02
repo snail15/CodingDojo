d97b1da7a9d1a15d343112809f892099bd9d1118
Sungin Jung <jungs@uchicago.edu>
2017-08-02 12:45:43 -0500

    add validation to DojoSurvey flask

--- Python/flask/flask_fundamental/DojoSurvey/server.py
+++ Python/flask/flask_fundamental/DojoSurvey/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash, session
 
 app = Flask(__name__)
+app.secret_key = "validation"
 
 @app.route('/')
 def index():
@@ -9,10 +10,22 @@ def index():
 @app.route('/result', methods=["POST"])
 def result():
     
+    error = 0
     name = request.form["name"]
     location = request.form["location"]
     language = request.form["language"]
     comment = request.form["comment"]
+    if len(name) < 1:
+        flash("Name is empty!")
+        error += 1
+    if len(comment) < 1:
+        flash("Actually, comment ain't optional")
+        error += 1
+    if len(comment) > 120:
+        flash("Comment too long")
+        error += 1
+    if error > 0:
+        return redirect("/")
 
     return render_template("result.html", name=name, location=location, language=language, comment=comment)
 
