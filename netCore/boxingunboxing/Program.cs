5ff5d1f4babeebfa7a9cdce7b329794c6b1e8984
Sungin Jung <jungs@uchicago.edu>
2017-09-01 16:51:23 -0500

    done

---
+++ netCore/boxingunboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingunboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> randomList = new List<object>();
+            randomList.Add(7);
+            randomList.Add(28);
+            randomList.Add(-1);
+            randomList.Add(true);
+            randomList.Add("chair");
+            System.Console.WriteLine(randomList);
+            int sum = 0;
+            foreach (object item in randomList)
+            {
+                if(item is int){
+                    sum += (int)item;
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
