a83e67d36209500a1aeff1141c8e65d3eab5087d
Sungin Jung <jungs@uchicago.edu>
2017-09-02 01:53:21 -0500

    done

---
+++ netCore/human/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class Human {
+            string name;
+            int strength = 3;
+            int intelligence = 3;
+            int dexterity = 3;
+            int health = 100;
+            
+            public Human(string humanName){
+                name = humanName;
+            }
+
+            public Human(string humanName, int strength, int intelligence, int dexterity, int health){
+                this.name = humanName;
+                this.strength = strength;
+                this.intelligence = intelligence;
+                this.dexterity = dexterity;
+                this.health = health;
+            }
+
+            private void attack(Human human){
+                human.health -= (this.strength * 5);
+            }
+        }
+    }
+}
